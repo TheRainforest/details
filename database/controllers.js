592c6e219ee16e9c35d42b777bf4848cafe2b50e
Jesse Beard <jjbeard@protonmail.com>
2020-05-20 20:25:43 -0700

    added delete path to API

--- database/controllers.js
+++ database/controllers.js
@@ -1,6 +1,6 @@
 const Item = require('./database');
 
-exports.createDoc = (item, callback) => {
+exports.saveDoc = (item, callback) => {
   const newItem = new Item(item);
   newItem.save((err, item) => {
     if (err) {
@@ -15,6 +15,17 @@ exports.createDoc = (item, callback) => {
 
 exports.getDoc = (itemId, callback) => {
   Item.findOne({ id: itemId }, (err, item) => {
+    if (err) {
+      console.log(err);
+      callback(err, null);
+    } else {
+      callback(null, item);
+    }
+  });
+};
+
+exports.deleteDoc = (itemId, callback) => {
+  Item.findOneAndDelete({ id: itemId }, (err, item) => {
     if (err) {
       console.log(err);
       callback(err, null);
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const { getDoc, createDoc } = require('../database/controllers');
+const { getDoc, createDoc, deleteDoc } = require('../database/controllers');
 
 const PORT = 3002;
 
@@ -14,10 +14,11 @@ app.use(express.static(path.join(__dirname, '/../public')));
 app.post('/items/', (req, res) => {
   createDoc(req.body, (err) => {
     if (err) {
+      console.log(`error saving: ${err}`)
       res.status(400);
       res.end();
     } else {
-      console.log('item was created with id: ', req.body.id);
+      console.log('saved id: ', req.body.id);
       res.status(201);
       res.end();
     }
@@ -28,9 +29,22 @@ app.get('/items/:id', (req, res) => {
   const itemId = req.params.id;
   getDoc(itemId, (err, success) => {
     if (err) {
-      console.log(err);
+      console.log(`error fetching: ${err}`);
       res.sendStatus(404).end();
     } else {
+      res.send(success).end();
+    }
+  });
+});
+
+app.delete('/items/:id', (req, res) => {
+  deleteDoc(req.params.id, (err, success) => {
+    if (err) {
+      console.log(`error deleteing: ${err}`);
+      res.sendStatus(404).send('cannot delete');
+    } else {
+      console.log(`deleted id: ${success.id}`);
+      res.status(200);
       res.send(success).end();
     }
   });
